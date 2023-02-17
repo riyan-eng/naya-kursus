86efc12f88a25693ee1ede55b8bf7447ff191b09
riyan-eng <fbriyant11@gmail.com>
2023-02-17 11:29:15 +0700

    temukan kelipatan 5 terdekat

--- 05-03/latihan.go
+++ 05-03/latihan.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sort"
 )
 
@@ -19,8 +20,24 @@ func Gimme(array [3]int) int {
 	return 0
 }
 
+func RoundToNext5(n int) int {
+	lala := math.Ceil(float64(n) / 5)
+	if lala == -0 {
+		lala = 0
+	}
+	return int(lala) * 5
+}
+
 func main() {
-	fmt.Println(Gimme([3]int{2, 3, 1}))
-	fmt.Println(Gimme([3]int{5, 10, 14}))
-	fmt.Println(Gimme([3]int{1, 3, 4}))
+	// fmt.Println(Gimme([3]int{2, 3, 1}))
+	// fmt.Println(Gimme([3]int{5, 10, 14}))
+	// fmt.Println(Gimme([3]int{1, 3, 4}))
+
+	fmt.Println(RoundToNext5(2))
+	fmt.Println(RoundToNext5(3))
+	fmt.Println(RoundToNext5(6))
+	fmt.Println(RoundToNext5(11))
+	fmt.Println(RoundToNext5(21))
+	fmt.Println(RoundToNext5(-5))
+	fmt.Println(RoundToNext5(-2))
 }
