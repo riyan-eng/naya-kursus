8abb0efef6d98e33f9da15806236a952dac14fe9
riyan-eng <fbriyant11@gmail.com>
2023-02-22 09:55:22 +0700

    code war

--- 06-02/tugas.go
+++ 06-02/tugas.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
 
+func solve(str string) string {
+	// Your code here and happy coding!
+	// var lowerCount int
+	var upperCount int
+	for _, val := range str {
+		if unicode.IsUpper(val) {
+			upperCount++
+		}
+	}
+
+	if upperCount > len(str)/2 {
+		return strings.ToUpper(str)
+	}
+
+	return strings.ToLower(str)
+}
+
+func main() {
+	kata := "codE"
+	fmt.Println(solve(kata))
+
+	// fmt.Println(lowerCount)
+	// fmt.Println(upperCount)
 }
